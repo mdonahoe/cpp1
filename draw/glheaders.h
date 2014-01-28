3df2faa865920056fb4daa81b4c34c54e207d942
Matt Donahoe <matt.donahoe@gmail.com>
2014-01-28 14:19:05 -0800

    conditional opengl includes for OSX vs linux

--- draw/glheaders.h
+++ draw/glheaders.h
@@ -1,2 +1,8 @@
-#include <GLUT/GLUT.h>//GLUT Library, will make you life easier
-#include <OpenGL/OpenGL.h>//OpenGL Library
+#ifdef __APPLE__
+    #include <GLUT/GLUT.h>//GLUT Library, will make you life easier
+    #include <OpenGL/OpenGL.h>//OpenGL Library
+#else
+    #include <GL/glut.h>
+#endif
+
+
